f3c04de918c5b7a9c27a7022dc9273123096041e
Fitsyu Two <fitsyu2@gmail.com>
2019-12-25 00:27:22 +0700

    my big commitment

---
+++ widget.cpp
@@ -0,0 +1,140 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QtDebug>
+#include <QFileDialog>
+#include <QPrintPreviewWidget>
+#include <QPainter>
+
+#include <poppler-qt5.h>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+
+    setupLayout();
+
+    setupInterplays();
+
+    // remove this ato de ne
+    loadDocument("/Users/fitsyu/Desktop/cnds.pdf");
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::setupLayout() {
+
+    ui->openDocumentButton->setFixedHeight(40);
+}
+
+void Widget::setupInterplays() {
+
+    connect(ui->openDocumentButton, &QPushButton::clicked, this, &Widget::openDocument);
+
+    connect(this, &Widget::documentFileNameDidSet, this, &Widget::loadDocument);
+
+    connect(this, &Widget::documentDidLoad, this->ui->previewWidget, &QPrintPreviewWidget::updatePreview );
+
+    connect(ui->previewWidget, &QPrintPreviewWidget::paintRequested, this, &Widget::previewPages);
+
+    connect(this, &Widget::documentDidRendered, this, &Widget::doneRendering);
+
+    connect(ui->printButton, &QPushButton::clicked, ui->previewWidget, &QPrintPreviewWidget::print);
+}
+
+void Widget::openDocument() {
+
+    QString fileName = "";
+
+    fileName = QFileDialog::getOpenFileName(this,
+                                            "Select a document file",
+                                            "",
+                                            "Offices (*.doc, *.docx, *.odt); PDF (*.pdf)"
+                                            );
+
+    if (fileName.count() > 0) {
+
+        ui->documentFileNameLabel->setText(fileName);
+
+        // done
+        emit documentFileNameDidSet(fileName);
+    }
+}
+
+void Widget::loadDocument(QString fileName) {
+
+    Poppler::Document* document = Poppler::Document::load(fileName);
+    if (!document || document->isLocked()) {
+        // ... error message ....
+        delete document;
+        return;
+    }
+
+    // Paranoid safety check
+    if (document == nullptr) {
+        // ... error message ...
+        return;
+    }
+
+    // save
+    this->pdfDoc = document;
+
+    // done
+    emit documentDidLoad();
+}
+
+void Widget::previewPages(QPrinter* printer) {
+
+
+    if ( pdfDoc == nullptr ) return;
+
+
+    int pagesCount = pdfDoc->numPages();
+
+    ui->pagesCountLabel->setText(QString("Pages: %1").arg(pagesCount));
+
+
+    QPainter painter(printer);
+
+    // render each page
+    for (int p=0; p<pagesCount; p++) {
+
+        // get image
+        Poppler::Page* page = pdfDoc->page(p);
+        QImage image = page->renderToImage();
+
+
+        // sizing
+        QRect rect = painter.viewport();
+        QSize size = image.size();
+        size.scale(rect.size(), Qt::KeepAspectRatio);
+        painter.setViewport(rect.x(), rect.y(), size.width(), size.height());
+        painter.setWindow(image.rect());
+
+        // render
+        painter.drawImage(0, 0, image);
+
+        if (p != pagesCount-1)
+            printer->newPage();
+
+        // done for this page
+        delete page;
+    }
+
+    painter.end();
+
+    // done
+    emit documentDidRendered();
+}
+
+void Widget::doneRendering() {
+
+    qDebug() << "Done.";
+    delete pdfDoc;
+}
---
+++ widget.h
@@ -0,0 +1,44 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QPrinter>
+#include <poppler-qt5.h>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    void setupLayout();
+    void setupInterplays();
+
+// =============================================
+
+signals:
+    void documentFileNameDidSet(QString);
+    void documentDidLoad();
+    void documentDidRendered();
+
+private slots:
+    void openDocument();
+    void loadDocument(QString fileName);
+    void previewPages(QPrinter *printer);
+    void doneRendering();
+
+// =============================================
+
+private:
+    Ui::Widget *ui;
+    Poppler::Document *pdfDoc;
+};
+
+#endif // WIDGET_H
